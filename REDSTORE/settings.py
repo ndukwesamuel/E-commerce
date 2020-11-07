b9320538bfbe74ba57b41c8dff839f492acf7ac7
ndukwesamuel23 <ndukwesamuel23@gmail.com>
2020-11-07 15:35:39 +0100

    my se

--- REDSTORE/settings.py
+++ REDSTORE/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,4 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
